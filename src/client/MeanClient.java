0ffcb5782a4b5f99d51c5830b3e366d13de5f250
cbudtz <chrbudtz@gmail.com>
2015-03-04 19:11:53 +0100

    IT WORKS!!!!

--- src/client/MeanClient.java
+++ src/client/MeanClient.java
@@ -4,23 +4,21 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 import server.ICalculator;
 
 public class MeanClient {
 
 	public static void main(String[] args) {
-		double value = 0;
-		System.setSecurityManager(new SecurityManager());
 
 		try
 		{
-			ICalculator obj = (ICalculator) Naming.lookup("rmi://localhost/Calculator");
-			System.out.println(obj.calculateMean());
+			Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+			ICalculator stub = (ICalculator) registry.lookup("Calculator");
+			System.out.println(stub.calculateMean());
 		} catch (RemoteException e) {
-			e.printStackTrace();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (NotBoundException e) {
 			// TODO Auto-generated catch block
--- src/server/Calculator.java
+++ src/server/Calculator.java
@@ -1,27 +1,17 @@
 package server;
 
-import java.rmi.Naming;
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
-import java.rmi.server.RMIClientSocketFactory;
-import java.rmi.server.RMIServerSocketFactory;
 import java.rmi.server.UnicastRemoteObject;
+import java.security.Policy;
 
-public class Calculator extends UnicastRemoteObject implements ICalculator {
+public class Calculator implements ICalculator {
 
 	public Calculator() throws RemoteException {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
-	public Calculator(int arg0) throws RemoteException {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
-
-	public Calculator(int arg0, RMIClientSocketFactory arg1,
-			RMIServerSocketFactory arg2) throws RemoteException {
-		super(arg0, arg1, arg2);
-		// TODO Auto-generated constructor stub
-	}
 
 	public double calculateMean() throws RemoteException {
 		// TODO Auto-generated method stub
@@ -29,10 +19,11 @@ public class Calculator extends UnicastRemoteObject implements ICalculator {
 	}
 
 	public static void main(String[] args) {
-		System.setProperty("java.rmi.server.hostname","localhost");
 		try {
 			Calculator calc = new Calculator();
-			Naming.rebind("rmi://localhost:1099/Calculator", calc);
+			ICalculator stub = (ICalculator) UnicastRemoteObject.exportObject(calc, 0);
+			Registry registry = LocateRegistry.createRegistry(1099);
+			registry.bind("Calculator", stub);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
