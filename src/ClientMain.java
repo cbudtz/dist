1c12142f8a7af89306eef7e8b03b92128a3df206
cbudtz <Splat1477>
2015-03-05 09:45:21 +0100

    Importing sensor code

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -2,7 +2,7 @@
 public class ClientMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub Start the RMI client
+		// TODO Auto-generated method stub
 
 	}
 
--- src/SensorMain.java
+++ src/SensorMain.java
@@ -1,8 +1,17 @@
+import java.util.LinkedList;
+
+import sensor.ClientController;
+
 
 public class SensorMain {
-
+	static String server = "127.0.0.1"; //args[0];
+	static int port = 9999; //Integer.parseInt(args[1]);
+	static volatile LinkedList<String> queue = new LinkedList<>();
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub Start a number of Sensors
+		ClientController ctrl = new ClientController(server,port);
+		ctrl.start();
 
 	}
 
